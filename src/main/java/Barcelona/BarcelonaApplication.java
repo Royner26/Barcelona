6be871f06abf7864b907c7ee2206b7eef7c75160
RoynerBadilla <RoynerBadilla@RoynerBadilla>
2023-06-05 03:34:17 -0600

    Creación tarea2

---
+++ src/main/java/Barcelona/BarcelonaApplication.java
@@ -0,0 +1,13 @@
+package Barcelona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcelonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcelonaApplication.class, args);
+	}
+
+}
